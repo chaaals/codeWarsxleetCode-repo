acc62e0ad8c7e8a044c05140e638ebc5aaca2dc4
chaaals <charlesching0808@gmail.com>
2022-03-27 00:10:06 +0800

    Update: included with best practice using str.replace() method.

--- createPhoneNumber.js
+++ createPhoneNumber.js
@@ -1,16 +1,22 @@
 "use strict";
 
-const numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-let str = "";
-
+const numArr = [1, 2, 3, 4, 5, 8, 7, 8, 9, 0];
+let phoneNumber = "(xxx) xxx-xxxx";
+// best practice
 const createPhoneNumber = (numArr) => {
-  let str = "";
   for (let i = 0; i < numArr.length; i++) {
-    str += numArr[i];
+    phoneNumber = phoneNumber.replace("x", numArr[i]);
   }
-  return `(${str[0] + str[1] + str[2]}) ${str[3] + str[4] + str[5]}-${
-    str[6] + str[7] + str[8] + str[9]
-  }`;
+  return phoneNumber;
 };
 
-console.log(createPhoneNumber(numArr));
+// Initial solution
+// const createPhoneNumber = (numArr) => {
+//   let str = "";
+//   for (let i = 0; i < numArr.length; i++) {
+//     str += numArr[i];
+//   }
+//   return `(${str[0] + str[1] + str[2]}) ${str[3] + str[4] + str[5]}-${
+//     str[6] + str[7] + str[8] + str[9]
+//   }`;
+// };
