1e3706a4753388016de7500135e3f062e24fa831
chaaals <charlesching0808@gmail.com>
2022-04-15 22:02:20 +0800

    Codewars: Update, finished findOddInt

--- findOddInt.js
+++ findOddInt.js
@@ -10,11 +10,12 @@ const findOdd = function (a) {
 
   const oddInt = Object.values(objOccurance)
     .map((value) => value)
-    .reduce((acc, value) => (acc > value ? acc : value));
+    .sort((a, b) => b - a)
+    .find((value) => value % 2 !== 0);
 
-  return Object.keys(objOccurance).find(
-    (key) => objOccurance[`${key}`] === oddInt
+  return Number(
+    Object.keys(objOccurance).find((key) => objOccurance[`${key}`] === oddInt)
   );
 };
 
-console.log(findOdd([1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1]));
+console.log(findOdd([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5]));
